9e642975d56c761f760e70679aee030572b0bf21
Manish Devgan <manish.nsit8@gmail.com>
2018-01-21 17:08:07 +0000

    Mew-Python

---
+++ MEW.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+import platform
+import json
+import os
+import re
+
+class MEW(object):
+
+	package_manager = None
+	command = None
+	arguments = None
+	version = None
+	distro = None
+	_package_manager = None
+	translated = None
+	_managers = ['apt-get','apt','pacman','dnf','rpm','zypper','emerge','equery']
+	_superDistros = ['debian','ubuntu','redhat','fedora','sles','opensuse','gentoo']
+
+	def __init__(self, package_manager, command, arguments):
+
+		self.package_manager = package_manager # package manager in cli
+		self.command = command # command in cli
+		self.arguments = arguments # args in cli
+		self.version = platform.version().lower() # version of linux
+		self.distro = self.getDistro() # linux distro 
+		self._package_manager = self.getPackageManager() # package manager for this distro
+		self.translated = self.translate()
+
+	def val_pac_manager(self):
+
+		if self.package_manager in _managers:
+			return True
+		return False
+
+	def getDistro(self):
+
+		for distro in self._superDistros:
+			if distro in self.version:
+				return distro
+
+	def getPackageManager(self):
+
+		distroData = json.load(open(os.path.join(os.path.realpath(__file__).replace("MEW.pyc","").replace("MEW.py","") ,"data", "distro.json")))
+
+		if distroData[self.distro]:
+			return distroData[self.distro]
+
+	def translate(self):
+
+		pacman_file = os.path.join(os.path.realpath(__file__).replace("MEW.pyc","").replace("MEW.py",""), "data", self.package_manager + ".json")
+		pacman_data = json.load(open(pacman_file)) # load json of the package manager
+
+		if re.match("-Sy+u",self.command):
+			self.command = "-Syu"
+
+		try:
+			if not pacman_data[self.command]:
+				return None
+			return self.format(pacman_data[self.command][self.distro])
+		except KeyError:
+			return None
+
+	def format(self, translate):
+
+		if "$vars" in translate:
+			if self.checkArgs():
+				return translate.replace("$vars", ''.join(self.arguments).replace("@",' ')[1:])
+			else:
+				translate = None
+		return translate	
+
+	def checkArgs(self):
+
+		if not len(self.arguments):
+			print "Error : Arguments must start with '@'"
+			return False
+
+		for args in self.arguments:
+			if not args.startswith("@"):
+				print "Error : Arguments must start with '@'"
+				return False
+
+		return True
+
+
+
+"""
+.replace("MEW.pyc","").replace("MEW.py","")
+replaces both of them so that we can get the directory 
+from the MEW.py file. .pyc is replaced because after it is 
+a used once then .pyc file is the main __file__
+"""
--- README.md
+++ README.md
@@ -1,20 +1,3 @@
-# Mew
-
-package manger translator
-
-![alt text](https://github.com/fossasia/mew/blob/master/graphics/logo.png "kind of a logo")
-
-## Any package manger command on any distro
-
-## How to install
-
-    * clone `https://github.com/fossasia/mew.git`
-    * cd mew
-    * ./mew
-
-Arch | pacman | Q | Qc | Qi | Ql | Qm | Qo | Qp | Qs | Qu | Rn | Rns | Rs | S | Sc/Scc | Si | Sii | Sl | Ss | Syu | Sw | Sy | U
-:--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--: | :--:
-**Debian / Ubuntu** | apt / apt-get | dpkg -l | changelog | dpkg -s / aptitude show| dpkg -L|aptitude purge '~o'| dpkg -S / dlocate| dpkg -I | aptitude search '~i(~n $name\|~d $description)'| apt-get upgrade -> n| | | apt remove | apt install --reinstall |  apt autoclean/ apt clean |apt-cache show / aptitude show |apt-cache rdepends / aptitude search ~D$pattern|apt-cache dumpavail apt-cache dump (Cache only) apt-cache pkgnames |apt search| apt update && apt upgrade | apt install --download-only (into the package cache) apt download (bypass the package cache)|  apt-get update| apt install|
-**Red Hat / Fedora** | dnf/rpm |qa |   -q --changelog|qi|ql |package-cleanup --orphans|rpm -qf (installed only) or dnf provides (everything) | rpm -qp |  rpm -qa '*\<str>\*'| dnf list updates, dnf check-update | | |dnf remove   |  dnf reinstall | dnf clean all| dnf info|dnf repoquery --alldeps --whatrequires  |  dnf list available|dnf search |  dnf upgrade | dnf download |dnf clean expire-cache && dnf check-update | dnf install |
-**SLES/openSUSE**| zypper/rpm |search -s/ qa|   -q --changelog|info/qi |ql | | zypper search -f  | | |zypper list-updates zypper patch-check (just for patches)  | | |zypper remove / zypper rm | zypper install --force |  zypper clean | zypper info| zypper search --requires |zypper packages| zypper search zypper se [-s] | zypper update zypper up| zypper --download-only |zypper refresh zypper ref| zypper in|
-**Gentoo** | emerge/equery |  -e world  |  changes -f  |-pv and -S|files | | equery belongs| |eix -S -I |emerge -uDNp world  | | |emerge -C | emerge -1O  |  eclean distfiles |emerge -pv and emerge -S |equery depends |emerge -ep world  |emerge -S  |emerge -u world |emerge --fetchonly |emerge --sync;layman -S  | emerge |
+# Mew-Python
+Mew Python is a python Wrapper for FOSSASIA's MEW.
+It will be done here but will be send as a PR to the original FOSSASIA's MEW.
